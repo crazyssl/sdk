df28be59eda9002da90b981be9268b6d3f51fbbf
xiaohui.lam <xiaohui.lam@icloud.com>
2019-08-20 01:58:11 +0800

    fixbug

--- src/Resources/Callback.php
+++ src/Resources/Callback.php
@@ -6,6 +6,6 @@ use Crazyssl\Resources\Abstracts\BaseResource;
 /**
  * @method void parse(\Closure $callback) PUSH回调,支持一个方法,方法入参分别为 ['cert_issued', $domains, $crt, $ca, $not_before, $not_after, $input]
  */
-class Callback extends BaseResouce
+class Callback extends BaseResource
 {
 }
