9dcb1267825068b36515fc21b7eb585125236666
Xiaohui Lam <xiaohuilam@users.noreply.github.com>
2019-12-21 18:00:33 +0800

    增加API响应字段

--- src/Response/GetSSLDetailsResponse.php
+++ src/Response/GetSSLDetailsResponse.php
@@ -4,6 +4,14 @@ namespace Crazyssl\Response;
 
 use Crazyssl\Response\Interfaces\BaseResponse;
 
+/**
+ * @property string $cert_code
+ * @property string $ca_code
+ * @property int $certificate_delivery_time
+ * @property int $reissue
+ * @property string $created_at
+ * @property string $reissued_at
+ */
 class GetSSLDetailsResponse extends AddSSLOrderResponse implements BaseResponse
 {
 }
